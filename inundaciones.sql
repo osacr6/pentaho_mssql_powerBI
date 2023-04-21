958457e40c02d82312f8f75a8aaf0a9e3eb4845e
Daniel Murillo <osacr6@gmail.com>
2023-04-21 00:50:09 -0600

    inundaciones

--- inundaciones.sql
+++ inundaciones.sql
@@ -9,23 +9,23 @@ GO
 
 --
 CREATE TABLE dbo.floods (
-    register_num VARCHAR(255) PRIMARY KEY,
+    register_num INTEGER PRIMARY KEY,
     date_began VARCHAR(255),
     date_ended VARCHAR(255),
-    duration_days VARCHAR(255),
-    deads  VARCHAR(255),
-    displaced VARCHAR(255),
+    duration_days INTEGER,
+    deads  INTEGER,
+    displaced INTEGER,
     damage_usd VARCHAR(255),
-    severity VARCHAR(255),
-    affected_sq_km VARCHAR(255),
-    magnitude VARCHAR(255),
+    severity FLOAT,
+    affected_sq_km FLOAT,
+    magnitude FLOAT,
     main_cause VARCHAR(255),
     notes TEXT
 );
 
 --
 CREATE TABLE dbo.location (
-    register_num VARCHAR(255) PRIMARY KEY,
+    register_num INTEGER PRIMARY KEY,
     country VARCHAR(255),
     location_details TEXT,
     longitude VARCHAR(255),
