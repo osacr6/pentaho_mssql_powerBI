1f8ab545134b3b78d1f7499b5624ecba1b085450
Daniel Murillo <osacr6@gmail.com>
2023-04-26 22:26:26 -0600

    hechos

--- desastres.sql
+++ desastres.sql
@@ -2,6 +2,14 @@ USE G2_PROYECTO_G4_DESASTRES;
 GO
 
 --
+DROP TABLE IF EXISTS dbo.dim_tipoevento;
+GO
+CREATE TABLE dbo.dim_tipoevento (
+    ID INTEGER PRIMARY KEY,
+    nombre VARCHAR(255),
+);
+
+--
 DROP TABLE IF EXISTS dbo.dim_geografica;
 GO
 
@@ -11,32 +19,26 @@ CREATE TABLE dbo.dim_geografica (
     location_details TEXT,
     longitude VARCHAR(255),
     latitude VARCHAR(255)
-    --FOREIGN KEY (register_num) REFERENCES floods(register_num)
-);
-
---
-DROP TABLE IF EXISTS dbo.dim_tipoevento;
-GO
-
-CREATE TABLE dbo.dim_tipoevento (
-    ID INTEGER PRIMARY KEY,
-    nombre VARCHAR(255),
-);
-
--- 
-DROP TABLE IF EXISTS dbo.hechos_eventonatural;
-GO
-
-CREATE TABLE dbo.hechos_eventonatural (
-    ID VARCHAR(255),
-	nombre VARCHAR(255),
-    tipoevento INTEGER,
-	magnitud FLOAT,
-    muertos INTEGER,
-    fecha VARCHAR(255)
-    --FOREIGN KEY (register_num) REFERENCES floods(register_num)
 );
 
 SELECT * FROM dbo.dim_geografica;
 GO
 
+-- 
+DROP TABLE IF EXISTS dbo.hechos_eventonatural;
+GO
+CREATE TABLE dbo.hechos_eventonatural (
+    ID VARCHAR(255) PRIMARY KEY,
+	nombre VARCHAR(255),
+    tipoevento INTEGER,
+	magnitud FLOAT,
+    muertos INTEGER,
+    fecha VARCHAR(255),
+	location_id VARCHAR(255),
+	tipoevento_id INTEGER,
+);
+
+alter table hechos_eventonatural add foreign key (location_id) references dim_geografica(ID)
+GO
+alter table hechos_eventonatural add foreign key (tipoevento_id) references dim_tipoevento(ID)
+GO
